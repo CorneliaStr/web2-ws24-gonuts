78fec31a35981b5ff78f6c457bd13089c7007f43
Bollich <Kevin.Bollich@kvwl.de>
2024-11-06 12:15:19 +0100

    Spring initiliaze

---
+++ goNuts/src/main/java/de/fhdo/goNuts/GoNutsApplication.java
@@ -0,0 +1,13 @@
+package de.fhdo.goNuts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoNutsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoNutsApplication.class, args);
+	}
+
+}
