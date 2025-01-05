83d703e29af6e5741bf3ffa33c5bb23aadfedc8a
Kevin Bollich <kevin.bollich001@stud.fh-dortmund.de>
2025-01-05 22:47:13 +0100

    GraphQL Controller von Product und Tag erweitert.

--- goNuts/src/main/java/de/fhdo/goNuts/graphqlcontroller/GraphProductController.java
+++ goNuts/src/main/java/de/fhdo/goNuts/graphqlcontroller/GraphProductController.java
@@ -4,6 +4,7 @@ import de.fhdo.goNuts.domain.Product;
 import de.fhdo.goNuts.dto.ProductDTO;
 import de.fhdo.goNuts.interfaces.ProductService;
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
@@ -24,4 +25,9 @@ public class GraphProductController {
 
     @QueryMapping("product")
     public ProductDTO getProduct(@Argument Long id) {return this.productService.getProduct(id);}
+
+    @MutationMapping
+    public ProductDTO createProduct(@Argument("product") ProductDTO product) {
+        return this.productService.createProduct(product);
+    }
 }
